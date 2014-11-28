e5bbf1b52d71ec38cbd683fe22dba3f0136b12a6
Stephan Geulette <s.geulette@imio.be>
2014-11-28 12:24:08 +0100

    pep8 corrections

--- src/collective/dms/scanbehavior/behaviors/behaviors.py
+++ src/collective/dms/scanbehavior/behaviors/behaviors.py
@@ -9,48 +9,54 @@ from collective.dms.scanbehavior import _
 class IScanFields(model.Schema):
 
     model.fieldset(
-            'scan',
-            label=_(u'Scan'),
-            fields=('scan_id',
-                'pages_number',
-                'scan_date',
-                'scan_user',
-                'scanner',
-                ),
-        )
+        'scan',
+        label=_(u'Scan'),
+        fields=(
+            'scan_id',
+            'pages_number',
+            'scan_date',
+            'scan_user',
+            'scanner',
+        ),
+    )
 
     scan_id = schema.TextLine(
-        title=_(u'scan_id',
+        title=_(
+            u'scan_id',
             default=u'Scan id',
-            ),
+        ),
         required=False,
     )
 
     pages_number = schema.Int(
-        title=_(u'pages_number',
+        title=_(
+            u'pages_number',
             default=u'Pages numbers',
-            ),
+        ),
         required=False,
     )
 
     scan_date = schema.Datetime(
-        title=_(u'scan_date',
+        title=_(
+            u'scan_date',
             default=u'Scan date',
-            ),
+        ),
         required=False,
     )
 
     scan_user = schema.TextLine(
-        title=_(u'scan_user',
+        title=_(
+            u'scan_user',
             default=u'Scan user',
-            ),
+        ),
         required=False,
     )
 
     scanner = schema.TextLine(
-        title=_(u'scanner',
+        title=_(
+            u'scanner',
             default=u'scanner',
-            ),
+        ),
         required=False,
     )
 
